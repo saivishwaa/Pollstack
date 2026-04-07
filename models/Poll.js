86b597d731f23d122005f0837aacd7a9203496a2
Shaunak Shukla <shaunak.btmtcs1232841@nfsu.ac.in>
2026-04-07 09:20:00 +0530

    PollStack backend complete

---
+++ models/Poll.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const PollSchema = new mongoose.Schema({
+  question: {
+    type: String,
+    required: true
+  },
+  options: [
+    {
+      text: String,
+      votes: {
+        type: Number,
+        default: 0
+      }
+    }
+  ]
+  votedUsers: {
+  type: [String],
+  default: []
+}
+});
+
+module.exports = mongoose.model('Poll', PollSchema);
---
+++ server.js
@@ -0,0 +1,95 @@
+const Poll = require('./models/Poll');
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(express.json());
+
+// MongoDB connection (ONLY ONE)
+mongoose.connect('mongodb+srv://shaunakbtmtcs1232841_db_user:nnWueSMIyAwVZ61B@cluster0.5mgae9t.mongodb.net/pollstack')
+  .then(() => console.log("Database connected"))
+  .catch(err => console.log(err));
+
+// Test route
+app.get('/', (req, res) => {
+  res.send("PollStack Backend Running");
+});
+
+// Create Poll API
+app.post('/api/poll', async (req, res) => {
+  try {
+    const poll = new Poll(req.body);
+    await poll.save();
+    res.json(poll);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+//vote api
+app.post('/api/vote', async (req, res) => {
+  try {
+    const { pollId, optionIndex } = req.body;
+
+    const poll = await Poll.findById(pollId);
+
+    if (!poll) {
+      return res.status(404).json({ error: "Poll not found" });
+    }
+
+    // Get user IP
+    const userIp = req.ip;
+
+    // Check if already voted
+    if (poll.votedUsers.includes(userIp)) {
+      return res.status(400).json({ error: "You have already voted!" });
+    }
+
+    // Add vote
+    poll.options[optionIndex].votes += 1;
+
+    // Save user IP
+    poll.votedUsers.push(userIp);
+
+    await poll.save();
+
+    res.json(poll);
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Get Results API
+app.get('/api/results/:id', async (req, res) => {
+  try {
+    const poll = await Poll.findById(req.params.id);
+
+    if (!poll) {
+      return res.status(404).json({ error: "Poll not found" });
+    }
+
+    // Calculate total votes
+    const totalVotes = poll.options.reduce((sum, opt) => sum + opt.votes, 0);
+
+    // Prepare results with percentage
+    const results = poll.options.map(opt => ({
+      text: opt.text,
+      votes: opt.votes,
+      percentage: totalVotes === 0 ? "0%" : ((opt.votes / totalVotes) * 100).toFixed(0) + "%"
+    }));
+
+    res.json({
+      question: poll.question,
+      results: results
+    });
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Start server
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
